7ab90e14da183c0fb587741eb49e8b4858d04247
Eduardo Maciel <106564207+MacielBortoli@users.noreply.github.com>
2023-05-02 23:15:55 -0300

    Add files via upload

---
+++ Exercicio - Formação Javascript Developer/AulasJS/ex001.js
@@ -0,0 +1,6 @@
+var vel = 59
+    console.log(`A velocidade do seu veiculo é ${vel}KM/H`)
+        if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. MULTADO!`)
+        }
+    console.log(`Dirija sempre usando cinto de segurança!`)
